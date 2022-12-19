fb934132f672df75698aace9843e070e3f0ab802
Anupam Shekhar <ianupamshekhar@gmail.com>
2022-12-20 03:03:38 +0530

    Display Contract Count on Website

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,57 @@
+import { countReset } from "console";
 import { ethers } from "ethers";
 
-async function hasSigners(): Promise<boolean> {
-    //@ts-ignore
-    const metamask = window.ethereum;
-    const signers = await (metamask.request({method: 'eth_accounts'}) as Promise<string[]>);
-    return signers.length > 0;
+function getEth() {
+  // @ts.ignore
+  const eth = window.ethereum;
+  if (!eth) {
+    throw new Error("No ethereum provider found");
+  }
+  return eth;
 }
 
-async function requestAccess(): Promise<boolean> {
-    //@ts-ignore
-    const result = (await window.ethereum.request({ method: 'eth_requestAccounts' })) as string[];
-    return result && result.length > 0;
+async function hasAccounts() {
+  const eth = getEth();
+  const accounts = (await eth.request({ method: "eth_accounts" })) as string[];
+
+  return accounts && accounts.length;
 }
 
-async function getContract() {
-    const address = process.env.CONTRACT_ADDRESS;
+async function requestAccounts() {
+  const eth = getEth();
+  const accounts = (await eth.request({
+    method: "eth_requestAccounts",
+  })) as string[];
 
-    if (!(await hasSigners()) && !(await requestAccess())) {
-        console.log("You are in trouble, no one wants to play");
+  return accounts && accounts.length;
+}
+
+async function run() {
+  if (!(await hasAccounts()) && !(await requestAccounts())) {
+    throw new Error("No accounts found");
+  }
+  const counter = new ethers.Contract(
+  process.env.CONTRACT_ADDRESS,
+  [
+    "function count() public",
+    "function getCounter() public view returns (uint256)",
+  ],
+  new ethers.providers.Web3Provider(getEth())
+}
+ const el = document.createElement("div");
+ async function setCounter() {
+    el.innerHTML = await counter.getCounter();
+
+ }
+ setCounter();
+
+ const button = document.createElement("button");
+    button.innerText = "Increment";
+    button.onclick = async function() {
+      await counter.count();
+        setCounter();
     }
-
-    // @ts-ignore
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    const contract = new ethers.Contract(
-        address,
-        [
-            "function hello() public pure returns(string memory)",
-        ], // abi
-        provider
-    );
-
-    console.log("We have done it, time to call");
-    console.log(await contract.hello());
-}
-
-
-getContract();
+    document.body.appendChild(el);
+    document.body.appendChild(button);
+} 
+run();
